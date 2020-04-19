febff09f1e7a16b054a0740d16f859bcdc04fb1f
arunyogith <arunyogith111@gmail.com>
2020-04-19 09:18:53 +0530

    can see a single todo in /todo/:id

---
+++ app/controllers/concerns/test_controller.rb
@@ -0,0 +1,4 @@
+class Testcontroller < ApplicationController
+    def home
+    end
+end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,9 @@
 class TodosController < ApplicationController
     def index
-      render plain: "Hello, this is /todos!"
+      render plain: Todo.order(:due_date).map {|todo| todo.to_pleasant_string}.join("\n")
+    end
+    def show
+      id=params[:id]
+      render plain: "the id you wanted is #{id}"
     end
   end
