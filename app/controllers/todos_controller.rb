b49b1ca40c42bfd216afabd4f137b8e179284557
arunyogith <arunyogith111@gmail.com>
2020-05-02 22:53:35 +0530

    use the resource routing convention

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,12 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
     def index
       render plain: Todo.order(:due_date).map {|todo| todo.to_pleasant_string}.join("\n")
     end
     def show
       id=params[:id]
-      render plain: "the id you wanted is #{id}"
+      todo=Todo.find(id)
+      render plain: todo.to_pleasant_string
     end
     def create
       todo_text=params[:todo_text]
