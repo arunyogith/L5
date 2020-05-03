54bb173c5f62287179ec38c7d525ff4f3abad992
arunyogith <arunyogith111@gmail.com>
2020-05-03 14:03:04 +0530

    post request to create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
  def index
     render plain:  User.order(:id).map { |user| user.to_pleasant_string}.join("\n")
  end
+ def show 
+    id=params[:id]
+    user=User.find(id)
+    render plain: user.to_pleasant_string
+ end
+ def create
+    name=params[:name]
+    email=params[:email]
+    password=params[:password]
+    user=User.create!(
+        name: name,
+        email: email,
+        password: password
+    )
+    render plain: "new user created "
+ end
 end
