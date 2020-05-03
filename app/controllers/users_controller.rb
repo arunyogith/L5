7d9974b3486b509532d25872a7c29a113f8ae761
arunyogith <arunyogith111@gmail.com>
2020-05-03 14:24:52 +0530

    post request used for login check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
  def index
     render plain:  User.order(:id).map { |user| user.to_pleasant_string}.join("\n")
  end
@@ -17,5 +18,15 @@ class UsersController < ApplicationController
         password: password
     )
     render plain: "new user created "
+ end
+ def login
+    email=params[:email]
+    password=params[:password]
+    user = User.find_by "email = ? and password = ?", email, password
+    if (user)
+      render plain: "true"
+    else
+      render plain: "false"
+    end
  end
 end
